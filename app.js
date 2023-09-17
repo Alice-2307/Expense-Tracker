fdc599badfc7b2d9490a59f481114594862c7378
Alice-2307 <adityasharma23july@gmail.com>
2023-09-17 20:50:50 +0530

    fix

--- app.js
+++ app.js
@@ -33,9 +33,11 @@ app.use("/premium", premium);
 
 app.use("/password", forgotPassword);
 
+
 app.use((req,res)=> {
     console.log("url",req.url)
-    res.sendFile(path.join(__dirname, `public/${req.url}`))
+    // res.sendFile(path.join(__dirname, `public/${req.url}`))
+    express.static(path.join(__dirname, `public/${req.url}`))
 })
 
 User.hasMany(Expense);
